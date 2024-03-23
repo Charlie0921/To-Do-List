1acb63d03998d10ba5bb1f918afda4d824d8ce2a
Kunjoong Kim <kunmiddle02@gmail.com>
2024-03-23 03:43:15 +0000

    functions for Category and DueDate

--- script.js
+++ script.js
@@ -1,67 +1,105 @@
+const formElement = document.getElementById("myForm")
+
 function openForm() {
-  document.getElementById("myForm").style.display = "flex";
+  formElement.style.display = "flex";
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  formElement.style.display = "none";
 }
 
+
 var task_id = 0;
+const task_form = formElement.querySelector("#task_form")
+let inputNameWrapper = task_form.querySelector("#name_wrap #task_name")
+let inputDateWrapper = task_form.querySelector("#date_wrap #task_date")
 
 function newElement() {
   console.log("________________________________________")
-  const myForm = document.querySelector("#myForm")
-  const task_form = myForm.querySelector("#task_form")
-  let inputName = task_form.querySelector("#name_wrap #task_name").value;
-  console.log(inputName)
-  let inputDate = task_form.querySelector("#date_wrap #task_date").value
-  console.log(inputDate)
-  let inputCategory = task_form.querySelector('#Category [name="task_category"]:checked').value
-  console.log(inputCategory)
 
-
-  //Create new elements
+  //Create new element in the list
   const todoList = document.querySelector('.List');
   const newLi = document.createElement('li')
   const newdiv = document.createElement('div')
+
+  //Set new id for each task
   task_id = task_id + 1;
   newLi.className = "Task"
   newLi.id = "Task" + task_id;
   newdiv.className = "Task-Elements"
 
-  //Check
+  //Selector
   const selectDiv = document.createElement('div')
   selectDiv.className = "Select"
-  selectDiv.addEventListener('click', function(e) {
+  selectDiv.addEventListener('click', function() {
     selectDiv.style.backgroundColor = "#6b6666"
   });
 
-  //Choose category color
+  //Set name for each task
+  const nameDiv = document.createElement('div')
+  nameDiv.className = "Name"
+  nameDiv.textContent = inputNameWrapper.value
+
+  //Edit Button
+  const editDiv = document.createElement('img')
+  editDiv.className = "edit"
+  editDiv.src = "../img/edit.svg"
+  editDiv.addEventListener('click', function() {
+    openForm();
+
+  });
+
+  //Remove Button
+  const removeDiv = document.createElement('img')
+  removeDiv.className = "remove"
+  removeDiv.src = "../img/remove.svg"
+
+  removeDiv.addEventListener('click', function() {
+    newLi.parentNode.removeChild(newLi);
+  });
+
+  //Add Multiple elements
+  newdiv.appendChild(selectDiv)
+  newdiv.appendChild(Category())
+  newdiv.appendChild(nameDiv)
+  newdiv.appendChild(DueDate())
+  newdiv.appendChild(editDiv)
+  newdiv.appendChild(removeDiv)
+
+  newLi.appendChild(newdiv);
+
+  todoList.appendChild(newLi);
+  console.log(newLi.length)
+}
+
+function Category() {
+  let inputCategoryWrapper = task_form.querySelector('#Category [name="task_category"]:checked')
+
   const categoryDiv = document.createElement('div')
   categoryDiv.className = "Category"
 
-
-  if (inputCategory === "Red") {
+  if (inputCategoryWrapper.value === "Red") {
     categoryDiv.style.backgroundColor = "#D82525";
-  } else if (inputCategory === "Orange") {
+  } else if (inputCategoryWrapper.value === "Orange") {
     categoryDiv.style.backgroundColor = "#E37D04";
-  } else if (inputCategory === "Yellow") {
+  } else if (inputCategoryWrapper.value === "Yellow") {
     categoryDiv.style.backgroundColor = "#EDB900";
-  } else if (inputCategory === "Green") {
+  } else if (inputCategoryWrapper.value === "Green") {
     categoryDiv.style.backgroundColor = "#518918";
   } else {
     categoryDiv.style.backgroundColor = "#0044F4";
   }
 
-  const nameDiv = document.createElement('div')
-  nameDiv.className = "Name"
-  nameDiv.textContent = inputName
+  console.log("hello" + inputCategoryWrapper.value)
 
-  //Calculates D-Day
+  return categoryDiv
+}
+
+function DueDate() {
   const dueDiv = document.createElement('div')
   dueDiv.className = "Due"
   const todayDate = new Date();
-  const dueDate = new Date(inputDate);
+  const dueDate = new Date(inputDateWrapper.value);
 
   diff = dueDate - todayDate
   console.log(diff)
@@ -73,43 +111,5 @@ function newElement() {
   } else {
     dueDiv.textContent = "D-" + Math.abs(diffDate)
   }
-
-  //Edit Button
-  const editDiv = document.createElement('img')
-  editDiv.className = "edit"
-  editDiv.src = "../img/edit.svg"
-  editDiv.addEventListener('click', function() {
-    openForm();
-
-    //after opening editing page
-    let list = document.querySelector(".List")
-    let task = document.querySelector("#Task" + task_id)
-    let name = task.querySelector(".Name").value
-    let date = task.querySelector(".Due").value
-
-    //
-
-  });
-
-
-  //Remove Button
-  const removeDiv = document.createElement('img')
-  removeDiv.className = "remove"
-  removeDiv.src = "../img/remove.svg"
-
-  removeDiv.addEventListener('click', function() {
-    newLi.parentNode.removeChild(newLi);
-  });
-
-  newdiv.appendChild(selectDiv)
-  newdiv.appendChild(categoryDiv)
-  newdiv.appendChild(nameDiv)
-  newdiv.appendChild(dueDiv)
-  newdiv.appendChild(editDiv)
-  newdiv.appendChild(removeDiv)
-
-  newLi.appendChild(newdiv);
-
-  todoList.appendChild(newLi);
-  console.log(newLi.length)
+  return dueDiv
 }
